93544b35536ee4d6b93e27fb2335c748dcd1084b
EwuraAbena Antwi <ewuraabenaantwibosiako@gmail.com>
2025-04-29 12:20:14 +0000

    style grid and add hover effect

--- script.js
+++ script.js
@@ -1,7 +1,13 @@
 const container = document.querySelector("#container");
 
+
 for (let i = 1; i < 257; i++){
   let divs = document.createElement("div");
   divs.classList.add("aDiv");
   container.appendChild(divs);
+
+  divs.addEventListener("mouseenter", () => {
+    divs.style.backgroundColor = "red";
+  })
 }
+

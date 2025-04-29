7e0c775c35ddfdf37ba61e0d95a88f09b8351ff3
EwuraAbena Antwi <ewuraabenaantwibosiako@gmail.com>
2025-04-29 19:29:52 +0000

    add user feature to specify grid size with pixelated trace of mouse movement

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
 const container = document.querySelector("#container");
+const btn = document.querySelector("button");
+
 
 
 for (let i = 1; i < 257; i++){
@@ -11,3 +13,22 @@ for (let i = 1; i < 257; i++){
   })
 }
 
+btn.addEventListener("click", () => {
+  let userResponse = prompt("How many squares per side?");
+  while (userResponse < 1 || userResponse > 100) {
+    userResponse = prompt("Invalid request! Number must be between 1 and 100");
+  }
+  document.querySelectorAll("#container div").forEach(div => div.remove());
+  
+
+  for (let i =1; i <(userResponse* userResponse + 1); i++){
+    let newDivs = document.createElement("div");
+    newDivs.classList.add("newDiv");
+    newDivs.style.width = `${960/userResponse}px`;
+    container.appendChild(newDivs);
+    
+    newDivs.addEventListener("mouseenter", ()=> {
+      newDivs.style.backgroundColor = "red";
+    })
+  }
+})
